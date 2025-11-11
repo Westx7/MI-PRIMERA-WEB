ac4cda57a7a0be7e3d52eff0826cacba1826ef5a
Westx7 <luis71265550@gmail.com>
2025-11-11 12:16:19 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// --- script.js ---
+
+// Muestra un mensaje cuando se carga la página
+console.log("¡Bienvenido a mi primera web interactiva!");
+
+// Función para cambiar el color del fondo
+function cambiarColor() {
+    document.body.style.backgroundColor = "#d1f7ff"; // cambia el color del fondo
+    alert("¡Color de fondo cambiado! 🎨");
+}
